782d79f14c8ac678bc90da552cd584657feec8a1
IsraelTineo <israeltineo84@gmail.com>
2026-06-15 20:28:25 -0300

    change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.controlgasto.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.2"
+        versionCode = 8
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
